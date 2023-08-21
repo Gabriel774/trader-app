ce081bd054b43d79f425efd9341a6c294170c5ac
gabriel <gabrielsantossousa774@gmail.com>
2023-08-21 02:00:48 -0300

    removido preact

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
